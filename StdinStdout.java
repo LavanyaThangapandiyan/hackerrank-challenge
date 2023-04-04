74e9dcf66f64044d45274f1218c1f5767d34f97c
LavanyaThangapandiyan <128019444+LavanyaThangapandiyan@users.noreply.github.com>
2023-04-04 11:51:34 +0530

    Create StdinStdout.java

---
+++ StdinStdout.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class StdinStdout {
+
+    public static void main(String[] args)
+     {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+         
+    }
+}
